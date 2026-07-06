59a283350c884374936b096446660e7c2b64d043
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:12:20 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,28 @@ output "databox_edge_devices" {
   description = "All databox_edge_device resources"
   value       = azurerm_databox_edge_device.databox_edge_devices
 }
+output "databox_edge_devices_device_properties" {
+  description = "List of device_properties values across all databox_edge_devices"
+  value       = [for k, v in azurerm_databox_edge_device.databox_edge_devices : v.device_properties]
+}
+output "databox_edge_devices_location" {
+  description = "List of location values across all databox_edge_devices"
+  value       = [for k, v in azurerm_databox_edge_device.databox_edge_devices : v.location]
+}
+output "databox_edge_devices_name" {
+  description = "List of name values across all databox_edge_devices"
+  value       = [for k, v in azurerm_databox_edge_device.databox_edge_devices : v.name]
+}
+output "databox_edge_devices_resource_group_name" {
+  description = "List of resource_group_name values across all databox_edge_devices"
+  value       = [for k, v in azurerm_databox_edge_device.databox_edge_devices : v.resource_group_name]
+}
+output "databox_edge_devices_sku_name" {
+  description = "List of sku_name values across all databox_edge_devices"
+  value       = [for k, v in azurerm_databox_edge_device.databox_edge_devices : v.sku_name]
+}
+output "databox_edge_devices_tags" {
+  description = "List of tags values across all databox_edge_devices"
+  value       = [for k, v in azurerm_databox_edge_device.databox_edge_devices : v.tags]
+}
 
